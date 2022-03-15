be6dfa9ac1e0e3cefcd845aee25e44379d751856
ZoharRo <zozozohar@gmail.com>
2022-03-15 12:05:56 +0200

    basic carousel

--- src/task1/App.js
+++ src/task1/App.js
@@ -1,12 +1,26 @@
+import React, { useState, useEffect } from "react";
 import logo from '../logo.svg';
 import './App.css';
 import ImageCarousel from "./ImageCarousel";
+import { fetchImages } from "../api/index.js"
 
 function App() {
+
+  const [imgs, setImgs] = useState(null)
+
+  useEffect(() => {
+    getImgs()
+  }, [])
+
+  async function getImgs() {
+    const imgs = await fetchImages()
+    setImgs(imgs)
+  }
+
   return (
-      <div>
-        <ImageCarousel />
-      </div>
+    <div>
+      <ImageCarousel imgs={imgs} />
+    </div>
   );
 }
 
--- src/task1/ImageCarousel.js
+++ src/task1/ImageCarousel.js
@@ -1,7 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
+import { BsFillArrowLeftCircleFill, BsFillArrowRightCircleFill } from "react-icons/bs"
+
 import { fetchImageUrls } from "../api/index";
 
-const ImageCarousel = (props) => {
-    return <h1>Your code goes here</h1>;
+const ImageCarousel = ({ imgs }) => {
+    const [currImgIdx, setImg] = useState(0)
+    const imgsLength = imgs?.length
+
+    const nextImg = () => {
+        const nextImg = currImgIdx === imgsLength - 1 ? 0 : currImgIdx + 1
+        setImg(nextImg)
+    }
+
+    const prevImg = () => {
+        const prevImg = currImgIdx === 0 ? imgsLength - 1 : currImgIdx - 1
+        setImg(prevImg)
+    }
+
+
+
+    return <section className="img-carousel">
+        <BsFillArrowLeftCircleFill className="left-arrow" onClick={prevImg} />
+        <BsFillArrowRightCircleFill className="rigth-arrow" onClick={nextImg} />
+        <div className="img-container">
+            <img src={imgs[currImgIdx]} alt="loading img" />
+        </div>
+    </section>
 };
 export default ImageCarousel;
